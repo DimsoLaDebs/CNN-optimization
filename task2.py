2e9b70f6522949e9cbb0872931d6b30bbc94e6c6
Vadim_Lagresle <123890685+DimsoLaDebs@users.noreply.github.com>
2024-05-15 16:42:34 +0200

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,119 @@
+import pathlib
+import matplotlib.pyplot as plt
+import utils
+from torch import nn
+from dataloaders import load_cifar10
+from trainer import Trainer
+
+
+class ExampleModel(nn.Module):
+    def __init__(self, image_channels, num_classes = 10):
+        """
+        Is called when model is initialized.
+        Args:
+            image_channels. Number of color channels in image (3)
+            num_classes: Number of classes we want to predict (10)
+        """
+        super().__init__()
+        # TODO: Implement this function (Task  2a)
+        num_filters = 32  # Set number of filters in first conv layer
+        self.num_classes = num_classes
+        # Define the convolutional layers
+        self.feature_extractor = nn.Sequential(
+            nn.Conv2d(in_channels=image_channels, out_channels=num_filters, kernel_size=5, stride=1, padding=2),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=5, stride=1, padding=2),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=5, stride=1, padding=2),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+
+        # For 32x32 input, the total number of features going into the first fully connected layer is 128 * 4 * 4.
+        self.num_output_features = 128 * 4 * 4
+
+        # Initialize the fully connected layers
+        self.classifier = nn.Sequential(
+            nn.Linear(self.num_output_features, 64),
+            nn.ReLU(),
+            nn.Linear(64, num_classes)
+            # Note: Do not apply softmax here when using nn.CrossEntropyLoss
+        )
+
+
+    def forward(self, x):
+        """
+        Performs a forward pass through the model
+        Args:
+            x: Input image, shape: [batch_size, 3, 32, 32]
+        """
+        # TODO: Implement this function (Task  2a)
+
+        batch_size = x.shape[0]
+        out = x
+        expected_shape = (batch_size, self.num_classes)
+        assert out.shape == (
+            batch_size,
+            self.num_classes,
+        ), f"Expected output of forward pass to be: {expected_shape}, but got: {out.shape}"
+
+
+        # Pass the input through the feature extractor part (Convolutional layers)
+        x = self.feature_extractor(x)  # This applies all convolution and pooling layers
+
+        # Flatten the output from the feature extractor to a vector
+        x = x.view(x.size(0), -1)  # x.size(0) is the batch size
+
+        # Pass the flattened output through the classifier (Fully connected layers)
+        out = self.classifier(x)
+
+        # The softmax activation is not applied here. If needed, it can be applied outside the model.
+        return out
+
+
+def create_plots(trainer: Trainer, name: str):
+    plot_path = pathlib.Path("plots")
+    plot_path.mkdir(exist_ok=True)
+    # Save plots and show them
+    plt.figure(figsize=(20, 8))
+    plt.subplot(1, 2, 1)
+    plt.title("Cross Entropy Loss")
+    utils.plot_loss(
+        trainer.train_history["loss"], label="Training loss", npoints_to_average=10
+    )
+    utils.plot_loss(trainer.validation_history["loss"], label="Validation loss")
+    plt.legend()
+    plt.subplot(1, 2, 2)
+    plt.title("Accuracy")
+    utils.plot_loss(trainer.validation_history["accuracy"], label="Validation Accuracy")
+    plt.legend()
+    plt.savefig(plot_path.joinpath(f"{name}_plot.png"))
+    plt.show()
+
+
+
+
+def main():
+    # Set the random generator seed (parameters, shuffling etc).
+    # You can try to change this and check if you still get the same result!
+    utils.set_seed(0)
+    print(f"Using device: {utils.get_device()}")
+    epochs = 10
+    batch_size = 64
+    learning_rate = 5e-2
+    early_stop_count = 4
+    dataloaders = load_cifar10(batch_size)
+    model = ExampleModel(image_channels=3, num_classes=10)
+    trainer = Trainer(
+        batch_size, learning_rate, early_stop_count, epochs, model, dataloaders
+    )
+    trainer.train()
+    create_plots(trainer, "task2")
+
+
+if __name__ == "__main__":
+    main()
